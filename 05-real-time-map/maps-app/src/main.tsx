76c27c79032dfb0796ce604d091e2679086d056a
raulpenate <50472064+raulpenate@users.noreply.github.com>
2024-09-13 04:13:53 -0600

    🐛 I'm refactoring with a bug
    Yes, this is my passion

--- 05-real-time-map/maps-app/src/main.tsx
+++ 05-real-time-map/maps-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import MapsApp from './MapsApp.tsx'
 
+console.log(`Hi, I'm a bug 🐞👋`)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MapsApp />
